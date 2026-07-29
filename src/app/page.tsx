924ed997d128cf37a4e9e022073073a0f68b6c88
mesbahtoha <mesbahulalam017@gmail.com>
2026-07-29 14:18:45 +0600

    feat: setup shadcn ui and project design foundation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Container from "@/components/shared/container";
+
 export default function HomePage() {
   return (
     <main>
-      Home Page
+      <Container className="py-20">
+        <h1 className="text-4xl font-bold">
+          Welcome to GearUp
+        </h1>
+
+        <p className="mt-4 text-muted-foreground">
+          Sports & Outdoor Equipment Rental Platform
+        </p>
+      </Container>
     </main>
   );
 }
